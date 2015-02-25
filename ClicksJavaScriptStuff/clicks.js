e3af6dc4103cfd28ca4f50617e0c8543994fe34f
apushyteacher <schwarza@sfusd.edu>
2015-02-24 17:03:58 -0800

    k

--- ClicksJavaScriptStuff/clicks.js
+++ ClicksJavaScriptStuff/clicks.js
@@ -1,3 +1,27 @@
 /*global $*/
 /*jslint sloppy:true, browser: true, white: true*/
 $("h1.page-title").text("My code runs!");
+$(window).on('click', function (e) {
+    $('img.logo').css({left: e.pageX});
+});
+$(window).on('mousemove', function (e) {
+    $('img.logo').css({left: e.pageX,
+                       top: e.pageY});
+});
+$(window).on('click', function (e) {
+    $('.logo').css({left: e.pageX,
+                    top: e.pageY});
+    $('.logo').toggleClass('rotated');
+});
+var x = $(window).width() / 2;
+var y = $(window).height() / 2;
+var theta = 0;
+var vtheta = Math.PI / 180;
+var r = 60;
+setInterval(function () {
+    $('.logo').css({
+        'left': x + r * Math.cos(theta),
+        'top': y + r * Math.sin(theta)
+    });
+    theta = theta + vtheta;
+}, 20);
